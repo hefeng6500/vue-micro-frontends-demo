7736b762974b49da9da8404fe9b8acfd2263a993
hefeng6500 <2443992009@qq.com>
2021-01-02 21:46:07 +0800

    feat: 添加了lerna管理工程、eslint、babel、stylelint、prettier

---
+++ packages/@kit/test-a/src/index.js
@@ -0,0 +1,5 @@
+import _ from "lodash";
+
+export default function name() {
+  console.log("test-a1212", _);
+}
---
+++ packages/main-app/src/app-second/routes/index.js
@@ -0,0 +1,18 @@
+import View from "../container.vue";
+
+const routes = {
+  path: "/user-management",
+  component: View,
+  children: [
+    {
+      path: "user",
+      component: () => import("../components/user.vue")
+    },
+    {
+      path: "role",
+      component: () => import("../components/role.vue")
+    }
+  ]
+};
+
+export default routes;
